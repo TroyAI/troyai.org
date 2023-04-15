2be2e1f620171bbb7ee31570c95256968ee2ca33
Ivan Zimmerman <ivan.g.zimmerman@gmail.com>
2023-04-14 19:01:15 -0700

    root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'index/index'
+  root to: 'index#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
