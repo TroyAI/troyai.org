2d5838e5f8bed81bc10e0a58f8f09895bbbd8a01
Ivan Zimmerman <ivan.g.zimmerman@gmail.com>
2023-04-15 20:21:02 -0700

    polish index

--- app/javascript/typinganimation.js
+++ app/javascript/typinganimation.js
@@ -1,7 +1,7 @@
 
 var animationAlreadyPlayed = document.cookie.includes("typing_animation_already_played=True");
 var typedText = document.getElementById("club-description");
-var textToType = `We are a student run research club located at Troy High School Fullerton CA. We are passionate about machine learning and the future of Artificial Intelligence. We offer competition and hands on coding experience to teach and inspire others about machine learning. Through machine learning we research and develop applications to make our community a better place.`;
+var textToType = `We are a student run research club located at Troy High School Fullerton CA. We're passionate about machine learning and the future of Artificial Intelligence. We offer competition and hands on coding experience to teach and inspire others about machine learning. Through machine learning we research and develop applications to make our community a better place.`;
 
 function playTypingAnimation() {
     typedText.innerText = '';
