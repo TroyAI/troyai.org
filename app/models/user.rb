7d756055ff437175e3656e5c168311bd16ea0e0a
Ivan Zimmerman <ivan.g.zimmerman@gmail.com>
2023-04-16 10:41:58 -0700

    misc fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :ai_model
+
+  validates :email, format: { with: /\A\d+@fjuhsd\.org\z/, message: "must be an school email."}
 end
