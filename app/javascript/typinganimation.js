7bfa8b94325edb4a0245dd94bc9cbdef8eea008f
Ivan Zimmerman <ivan.g.zimmerman@gmail.com>
2023-04-16 15:04:59 -0700

    avni stuffs

--- app/javascript/typinganimation.js
+++ app/javascript/typinganimation.js
@@ -1,5 +1,4 @@
 
-var animationAlreadyPlayed = document.cookie.includes("typing_animation_already_played=True");
 var typedText = document.getElementById("club-description");
 var textToType = `We are a student run research club located at Troy High School Fullerton CA. We're passionate about machine learning and the future of Artificial Intelligence. We offer competition and hands on coding experience to teach and inspire others about machine learning. Through machine learning we research and develop applications to make our community a better place.`;
 
@@ -10,17 +9,11 @@ function playTypingAnimation() {
       if (textToType.length > 0) {
         typedText.innerHTML += textToType.charAt(0);
         textToType = textToType.substring(1);
-        setTimeout(typeText, 20);
+        setTimeout(typeText, 30);
       }
     }
     
     typeText();
 }
 
-// this'll be a problem if the club still exists in 2040
-if (animationAlreadyPlayed) {
-    typedText.innerText = textToType;
-} else {
-    document.cookie = "typing_animation_already_played=True; expires=Thu, 18 Dec 2040 12:00:00 UTC";
-    playTypingAnimation();
-}
+playTypingAnimation();
