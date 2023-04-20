02ed77f0f22d0b5a11bb624e403bd3eece7f9a77
Ivan Zimmerman <ivan.g.zimmerman@gmail.com>
2023-04-19 20:04:02 -0700

    model library

--- app/helpers/auth_token_helper.rb
+++ app/helpers/auth_token_helper.rb
@@ -1,2 +1,13 @@
 module AuthTokenHelper
+    def new_token_string
+        token_string = ""
+        characters = [*('a'..'z'), *('A'..'Z'), *('0'..'9')]
+        length = 16
+    
+        length.times do
+          token_string << characters.sample
+        end
+
+        token_string
+    end
 end
